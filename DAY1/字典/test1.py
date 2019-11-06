417603a9bd3881f06a9c72a7b3d0b01ccbc26a1a
RobertYCJ <597409258@qq.com>
2019-11-06 20:47:32 +0800

    1

--- DAY1/字典/test1.py
+++ DAY1/字典/test1.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 a = {"a":1,"b":2,"c":3,"d":2,"e":3,"f":2}
-#new_a = {v : k for k, v in a.items()}
+b = ()
+c = ()
 sum = 0
+#new_a = {v : k for k, v in a.items()}
+for k in a:
+    print(k)
+for v in a.values():
+    print(v)
+
+#for k,v in a:
+
+
 for i in a:
     sum = sum + a[i]
 print(sum)
