d2eed11a53fc6796b0acd570f18381b40a83b14a
RobertYCJ <597409258@qq.com>
2019-11-06 19:10:53 +0800

    1

--- DAY1/列表/test2.py
+++ DAY1/列表/test2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 a = [13,2,5,1,3]
 b = input()
+c = ""
 e = []
 f = 0
 
@@ -13,7 +14,10 @@ else:
     for num in a:
          if float(num) > float(b) :
             e.insert(f,num)
+            c = c + str(a.index(num))
+            c = c + ","
             f += 1
-    print(e)
+    print("值分别为：",e)
+    print("索引分别为：",c)
 
     
