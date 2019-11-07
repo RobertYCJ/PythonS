43f2f466a4c0c3b39e5e7b5eb1563063a91b31ba
RobertYCJ <597409258@qq.com>
2019-11-07 11:04:47 +0800

    2

--- DAY1/字典/test1.py
+++ DAY1/字典/test1.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
 a = {"a":1,"b":2,"c":3,"d":2,"e":3,"f":2}
-b = ()
-c = ()
+
+same = []
+reslt = []
+alist = []
+blist = []
 sum = 0
-#new_a = {v : k for k, v in a.items()}
-for k in a:
-    print(k)
-for v in a.values():
-    print(v)
+# new_a = {v : k for k, v in a.items()}
+# print(new_a)
 
-#for k,v in a:
 
+for key in a:
+    val = a[key]
+    if val in same:
+        if val in alist:
+            pass
+        else:
+            alist.append(val)
+    else:
+        # 没有相
+        same.append(val)
+
+b = len(alist)
+
+
+for i in alist:
+    for key in a:
+        if a[key] == i:
+            blist.append(key)
+        else:
+            pass
+    print (i,blist)
+    blist = []
+        
 
 for i in a:
     sum = sum + a[i]
